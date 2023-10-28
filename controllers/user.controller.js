03e55bacadead0d4a2cf1f59462219dfda04dcf5
sany <mahabubsany2@gmail.com>
2023-10-28 23:57:47 +0600

    Added controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,30 @@
 const db = require('../utils/prisma');
 
-exports.createUser = async (req, res) => {};
+exports.createUser = async (req, res) => {
+  try {
+    const payload = req.body;
+    const response = await db.user.create({
+      data: payload,
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        name: true,
+        role: true,
+      },
+    });
+    res.status(201);
+    res.send({
+      status: response?.id ? 'success' : 'failed',
+      message: response?.id
+        ? 'User created successfully'
+        : 'Failed to create user.',
+      data: response,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.getUser = async (_req, res, next) => {
   try {
@@ -10,8 +34,10 @@ exports.getUser = async (_req, res, next) => {
         username: true,
         email: true,
         name: true,
-        // avatar: true,
         role: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
       },
     });
     res.status(200);
@@ -25,7 +51,31 @@ exports.getUser = async (_req, res, next) => {
   }
 };
 
-exports.getUserById = async (req, res) => {};
+exports.getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        name: true,
+        role: true,
+      },
+    });
+    res.status(200);
+    res.send({
+      status: user?.id ? 'success' : 'failed',
+      message: user?.id ? 'User fetch successfully' : 'Failed to fetch user.',
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.updateUserById = async (req, res) => {};
 
